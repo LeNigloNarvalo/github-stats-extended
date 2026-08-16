9244f25d0474c6b191d4d541bf58061a5d89df65
LeNigloNarvalo <goujon.djovany@outlook.com>
2026-08-16 12:30:05 +0200

    feat: forward columns and total_bar params to top-langs card renderer

--- packages/core/src/api/top-langs.js
+++ packages/core/src/api/top-langs.js
@@ -36,6 +36,8 @@ export default async (
     hide_progress,
     hide_values,
     stats_format,
+    columns,
+    total_bar,
   },
   pat = null,
 ) => {
@@ -163,6 +165,8 @@ export default async (
         hide_progress: parseBoolean(hide_progress),
         hide_values: parseBoolean(hide_values),
         stats_format,
+        columns: columns !== undefined ? parseInt(columns, 10) : undefined,
+        total_bar: parseBoolean(total_bar),
       }),
     };
   } catch (err) {
